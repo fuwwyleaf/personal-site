be5887c3207709283e66037d718003bde4e011ab
Angel <146998068+fuwwyleaf@users.noreply.github.com>
2023-12-01 15:23:48 +0000

    Update presence.js

--- js/presence.js
+++ js/presence.js
@@ -1,7 +1,7 @@
 async function getPresence() {
   try {
     var fetched = await fetch(
-      "https://japi.rest/discord/v1/user/419958345487745035"
+      "https://japi.rest/discord/v1/user/898255050592366642"
     );
     var res = await fetched.json();
     var status = res.presence.status;
